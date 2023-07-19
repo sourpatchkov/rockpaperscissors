7bede44c2065c36729cc5b6323d499cff4062bc8
Alan Pachkovskiy <alanosnovnoy@gmail.com>
2023-07-18 23:54:23 -0400

    Created the brains of the rock paper scissors
    Mistakes while creating: did (x== a || b). Realized that second conditi-
    on b will always be correct since the condition itself is b == b always.
    Fix, x== b too.

---
+++ javascript.js
@@ -0,0 +1,42 @@
+function getcomputerchoice(){
+    let x = Math.floor(Math.random() * 3) +1
+    if(x == 1){
+        return "ROCK"
+    }
+    else if(x==2){
+        return "PAPER"
+    }
+    else{
+    return "SCISSORS"}
+}
+
+function round(userInput, computerSelection){
+    if(userInput=== "ROCK" && computerSelection === "SCISSORS"){
+    return "User won"
+    }
+    else if(userInput=== "PAPER" && computerSelection === "ROCK"){
+        return "User won"
+    }
+    else if(userInput=== "SCISSORS" && computerSelection === "PAPER"){
+        return "User won"
+    }
+    else if(userInput === computerSelection){
+        return "draw"
+    }
+    else{
+        return "User Lost"
+    }
+}
+
+const userInput = prompt("Pick, rock, paper, scissors").toUpperCase()
+const computerSelection = getcomputerchoice()
+
+if(userInput === "ROCK" || userInput === "PAPER" || userInput === "SCISSORS"){
+
+console.log(userInput)
+console.log(computerSelection)
+console.log(round(userInput,computerSelection))
+
+}
+else
+console.log("Incorrect input")
